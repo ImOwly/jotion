4a417d31f479a729cec871fe63af50fabfc217f6
Matt <mattz3044@outlook.com>
2024-06-14 15:47:58 -0700

    added jotion project

---
+++ app/(public)/(routes)/preview/[documentId]/layout.tsx
@@ -0,0 +1,12 @@
+const PublicLayout = ({
+    children
+} : {
+    children: React.ReactNode;
+}) => {
+    return (
+    <div className="h-full">
+        {children}
+    </div>);
+}
+ 
+export default PublicLayout;
